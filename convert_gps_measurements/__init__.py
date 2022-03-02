656dcb97b8c84a5fc81d7a90a7b8bb3c4bdcd248
Sýlvan Heuser <sylvan.heuser@gmx.net>
2022-03-02 03:07:28 +0100

    feat: Add proper argument handling and format listing

--- convert_gps_measurements/__init__.py
+++ convert_gps_measurements/__init__.py
@@ -12,26 +12,62 @@ output_format_map = {cls.__name__: cls for cls in output_formats}
 filters_map = {cls.__name__: cls for cls in filters}
 
 
+def list_formats(args):
+    """List input, output formats and filters."""
+    for name, list in (("INPUT FORMATS", input_format_map.keys()),
+                       ("OUTPUT_FORMATS", output_format_map.keys()),
+                       ("FILTERS", filters_map.keys())):
+        print(name)
+        for item in list:
+            print("  " + item)
+
+
+def convert(args):
+    """Convert one file format to another."""
+    raise NotImplementedError  # FIXME
+
+
 def main():
     """Handle arguments and call routines."""
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "--loglevel", default="WARNING", help="Loglevel", action="store"
     )
-    parser.add_argument("input_files", nargs="+", help="Input file(s)")
-    parser.add_argument(
+    subparsers = parser.add_subparsers(title="Commands")
+
+    plistformats = subparsers.add_parser(
+        "list-formats", aliases=["l", "list"], help=list_formats.__doc__
+    )
+    plistformats.set_defaults(func=list_formats)
+
+    pconvert = subparsers.add_parser(
+        "convert", aliases=["c"], help=convert.__doc__
+    )
+    pconvert.set_defaults(func=convert)
+    pconvert.add_argument("input_files", nargs="+", help="Input file(s)")
+    pconvert.add_argument(
         "--input-format",
         "-i",
         choices=input_format_map.keys(),
+        metavar="FORMAT",
+        default=("Csv", ),
         help="Input format"
     )
-    parser.add_argument(
-        "--filter", "-f", nargs="+", choices=filters_map.keys(), help="Filter"
+    pconvert.add_argument(
+        "--filter",
+        "-f",
+        nargs="+",
+        choices=filters_map.keys(),
+        metavar="FILTER",
+        default=("NoOp", ),
+        help="Filter"
     )
-    parser.add_argument(
+    pconvert.add_argument(
         "--output-format",
         "-o",
         choices=output_format_map.keys(),
+        metavar='FORMAT',
+        default=("Print", ),
         help="Output format"
     )
 
@@ -41,6 +77,9 @@ def main():
     if not isinstance(loglevel, int):
         raise ValueError("Invalid log level: {}".format(args.loglevel))
     logging.basicConfig(level=loglevel)
+
+    # call command function
+    args.func(args)
 
 
 if __name__ == "__main__":

235f304a0d1c8dbee9a57f93ca5fc7db754485c8
Sýlvan Heuser <sylvan.heuser@gmx.net>
2022-03-08 04:04:39 +0100

    feat: Add Survey2GIS TSV output

--- convert_gps_measurements/outputs.py
+++ convert_gps_measurements/outputs.py
@@ -1,11 +1,12 @@
 """Output formatters."""
 
+import argparse
+import csv
 import logging
 import pathlib
-import argparse
 from typing import Iterator
 
-from .geometries import Shape
+from .geometries import Line, Point, Polygon, Shape
 
 log = logging.getLogger(__name__)
 
@@ -27,8 +28,7 @@ class OutputFormat:
     def log(self):
         return logging.getLogger(f"{__name__}.{self.__class__.__name__}")
 
-
-    def format(self) -> Iterator[Shape]:
+    def format(self, shapes: Iterator[Shape]) -> None:
         raise NotImplementedError
 
 
@@ -37,3 +37,39 @@ class Print(OutputFormat):
     def format(self, shapes: Iterator[Shape]):
         for shape in shapes:
             print(shape)
+
+
+class Survey2GIS_TSV(OutputFormat):
+    SHAPE_CODES = {
+        Point: ".",
+        Line: "&",
+        Polygon: "@",
+    }
+
+    def format(self, shapes):
+        with self.output_path.open('w') as csvfile:
+            fieldnames = [
+                'id', 'name_plus_type', 'x_name', 'x', 'y_name', 'y', 'z_name',
+                'z'
+            ]
+            writer = csv.DictWriter(
+                csvfile,
+                fieldnames=fieldnames,
+                extrasaction='ignore',
+                dialect='excel-tab'
+            )
+
+            for shape in shapes:
+                num_of_points = len(shape.points)
+                for i, point in enumerate(shape.points):
+                    point_dict = point.dict()
+                    point_dict.update(point_dict["meta"])
+                    del point_dict["meta"]
+                    name = "{planum}_{object_code}_{object_number}".format(
+                        **point_dict
+                    )
+                    type_code = ""
+                    if i == (num_of_points - 1):
+                        type_code = self.SHAPE_CODES[shape.__class__]
+                    point_dict["name_plus_type"] = f"{name}{type_code}"
+                    writer.writerow(point_dict)
